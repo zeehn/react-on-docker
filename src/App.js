ed719dbc7f92313cd9cb24b7a9b7928d995186f6
Zakir Hussain <zack.mesacc@gmail.com>
2020-09-13 18:06:04 +0500

    Fix test error

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello from Docker-compose
+          Learn React
         </a>
       </header>
     </div>
